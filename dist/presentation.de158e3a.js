718493575a8a0e1ff058080ed6417523025b4b54
psolanoar <psolanoar@uoc.edu>
2024-04-17 17:43:31 +0200

    Added empty links in the detail, the gitignore and removed the folders from the file

--- dist/presentation.de158e3a.js
+++
@@ -1,9 +0,0 @@
-function getNewsletter() {
-    let userEmail = document.getElementById("email").value;
-    let send;
-    if (userEmail !== undefined && userEmail !== null) send = "Subscipci\xf3n realizada";
-    else send = "Email no apto";
-    document.getElementById("newsletter").innerHTML = send;
-}
-
-//# sourceMappingURL=presentation.de158e3a.js.map
